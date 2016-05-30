a38d96bc2896347d910582a641090ce07bc8896f
Toby Mac <tobymac@umich.edu>
2016-05-30 16:10:06 -0400

    added level loader and saver

--- src/snorri/world/Level.java
+++ src/snorri/world/Level.java
@@ -1,10 +1,20 @@
 package snorri.world;
 
-public class Level {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
-	private Tile[][] map;
-	private Vector dim;
+import snorri.main.Main;
+
+
+
+public class Level {
 	
+	private Tile[][]	map;
+	private Vector		dim;
+						
 	//TODO: load from file, not empty grid with parameters
 	//not that indexing conventions are Cartesian, not matrix-based
 	
@@ -15,6 +25,10 @@ public class Level {
 	
 	public void setTile(int x, int y, Tile t) {
 		map[x / Tile.WIDTH][y / Tile.WIDTH] = t;
+	}
+	
+	public void setTileRaw(int x, int y, Tile t) {
+		map[x][y] = t;
 	}
 	
 	public Tile getTile(int x, int y) {
@@ -29,4 +43,52 @@ public class Level {
 		return dim;
 	}
 	
+	public void load(String fileName) {
+		try {
+			byte[] b = new byte[4];
+			
+			FileInputStream is = new FileInputStream(fileName);
+			
+			is.read(b);
+			int width = ByteBuffer.wrap(b).getInt();
+			is.read(b);
+			int height = ByteBuffer.wrap(b).getInt();
+			
+			for (int i = 0; i < height; i++ ) {
+				byte[] b2 = new byte[width];
+				is.read(b2);
+			}
+			
+			is.close();
+		}
+		catch (FileNotFoundException ex) {
+			Main.error("Unable to open file '" + fileName + "'");
+		}
+		catch (IOException ex) {
+			Main.error("Error reading file '" + fileName + "'");
+		}
+	}
+	
+	public void save(String fileName) {
+		try {
+			FileOutputStream os = new FileOutputStream(fileName);
+			ByteBuffer b1 = ByteBuffer.allocate(4);
+			
+			byte[] buffer = b1.putInt(dim.getX()).array();
+			os.write(buffer);
+			buffer = b1.putInt(dim.getY()).array();
+			os.write(buffer);
+			
+			for (int i = 0; i < dim.getX(); i++ ) {
+				for (int j = 0; j < dim.getY(); j++ ) {
+					os.write(((byte) map[i][j].getId()) & 0xFF);
+				}
+			}
+			
+			os.close();
+		}
+		catch (IOException ex) {
+			Main.error("Error writing file '" + fileName + "'");
+		}
+	}
 }
