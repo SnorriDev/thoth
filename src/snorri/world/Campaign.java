63707f470c54d4a8ba72c7b8fc1e6c053f12ac54
Will M <vikingarnir.will@gmail.com>
2016-06-05 18:19:46 -0400

    textures here

---
+++ src/snorri/world/Campaign.java
@@ -0,0 +1,19 @@
+package snorri.world;
+
+public class Campaign implements Playable {
+
+	// TODO: store a bunch of worlds
+
+	@Override
+	public World getCurrentWorld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
