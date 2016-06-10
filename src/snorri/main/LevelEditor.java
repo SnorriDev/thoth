d6a06bb69145e85bb5b501beef597f7d31211b46
Toby Mac <tobymac@umich.edu>
2016-06-09 21:22:43 -0400

    removed unneeded imports

--- src/snorri/main/LevelEditor.java
+++ src/snorri/main/LevelEditor.java
@@ -1,23 +1,15 @@
 package snorri.main;
 
-import java.awt.FileDialog;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.File;
-import java.io.IOException;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
