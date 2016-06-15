350d8c30d62f3b5d1bed78bac5105b66bb502f65
Toby Mac <tobymac@umich.edu>
2016-06-15 16:02:52 -0400

    recommiting

---
+++ src/snorri/entities/Enemy.java
@@ -0,0 +1,156 @@
+package snorri.entities;
+
+import java.util.ArrayDeque;
+
+import snorri.inventory.Inventory;
+import snorri.inventory.Item;
+import snorri.inventory.Item.ItemType;
+import snorri.inventory.Orb;
+import snorri.inventory.Timer;
+import snorri.inventory.Weapon;
+import snorri.pathfinding.PathNode;
+import snorri.pathfinding.Pathfinder;
+import snorri.pathfinding.Pathfinding;
+import snorri.world.Vector;
+import snorri.world.World;
+
+public class Enemy extends Unit implements Pathfinder {
+
+	private static final long serialVersionUID = 1L;
+	private static final double APPROACH_MARGIN = 15;
+	private static final double CHANGE_PATH_MARGIN = 200;
+	
+	private Vector lastSeenPos;
+	private boolean recalculatingPath = false;
+	
+	protected double seekRange = 1000;
+	protected double attackRange = 300;
+	protected double attackDelay = 1;
+	
+	protected Inventory inventory;
+	protected Entity target;
+	
+	private ArrayDeque<PathNode> path;
+	
+	public Enemy(Vector pos, Entity target) {
+		super(pos);
+		this.target = target;
+		inventory = new Inventory(this);
+		setOrb((Orb) Item.newItem(ItemType.PELLET));
+		setWeapon((Weapon) Item.newItem(ItemType.SLING));
+	}
+	
+	public void setWeapon(Weapon weapon) {
+		weapon.setCustomTimer(new Timer(attackDelay));
+		inventory.setWeapon(weapon);
+	}
+	
+	public void setOrb(Orb orb) {
+		inventory.setOrb(0, orb);
+	}
+	
+	/**
+	 * does a grid raycast to detect unpathable terrain blocking the shot
+	 * @param target
+	 * 		the entity we're tryna cap
+	 * @return
+	 * 		whether terrain obstructs the shot
+	 */
+	public boolean canShootAt(World world, Entity target) {
+		
+		Vector step = target.pos.copy().sub(pos).normalize();
+		Vector tempPos = pos.copy();
+		
+		//I'm checking if pos and target.pos are both okay just in case we're in a wall
+		while (tempPos.distanceSquared(pos) <= target.pos.distanceSquared(pos)) {	
+		
+			if (! world.getLevel().isPathable(tempPos.copy().toGridPos())) {
+				return false;
+			}
+			
+			if (tempPos.distanceSquared(pos) > attackRange * attackRange) {
+				return false;
+			}
+			
+			tempPos.add(step);	
+		}
+
+		return true;
+		
+	}
+	
+	public void shootAt(World world, Entity e) {
+		if (inventory == null) {
+			return;
+		}
+		inventory.tryToShoot(world, this, Vector.ZERO.copy(), e.getPos().copy().sub(pos));
+	}
+	
+	@Override
+	public void update(World world, double deltaTime) {
+		
+		if (target != null) {
+			
+			if (path != null) {
+				
+				if (target.pos.distanceSquared(lastSeenPos) > CHANGE_PATH_MARGIN * CHANGE_PATH_MARGIN) {
+					stopPath(); //path will be recalculated if it's still in range
+				}
+				else if (canShootAt(world, target)) {
+					shootAt(world, target);
+				}
+				else {
+					follow(world, deltaTime);
+				}
+				
+			}
+			else if (target.pos.distanceSquared(pos) <= seekRange * seekRange && ! recalculatingPath) {
+				startPath();
+			}
+						
+		}
+		
+		inventory.update(deltaTime);
+		super.update(world, deltaTime);
+		
+	}
+
+	private void follow(World world, double deltaTime) {
+		
+		if (path.peek().getGlobalPos().distance(this.getPos()) < APPROACH_MARGIN) {
+			path.pop();
+		}
+		
+		if (path.isEmpty()) {
+			stopPath();
+			return;
+		}
+		
+		walkTo(world, path.peek().getGlobalPos());
+		
+	}
+
+	@Override
+	public void setPath(ArrayDeque<PathNode> stack) {
+		
+//		if (stack == null) {
+//			return;
+//		}
+		
+		path = stack;
+		recalculatingPath = false;
+		
+	}
+	
+	public void startPath() {
+		Pathfinding.setPathAsync(pos.copy().toGridPos(), target.pos.copy().toGridPos(), this);
+		lastSeenPos = target.pos.copy(); //don't put this in other thing
+		recalculatingPath = true;
+	}
+	
+	public void stopPath() {
+		path = null;
+		recalculatingPath = false;
+	}
+
+}
---
+++ src/snorri/pathfinding/Pathfinding.java
@@ -0,0 +1,103 @@
+package snorri.pathfinding;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+import snorri.world.Vector;
+import snorri.world.World;
+
+public class Pathfinding {
+	
+	//save array of costs from start
+	//everything is null, or etc.
+		
+	private static World world;
+	private static Vector dim;
+	
+	//reset with every call to findPath
+	private static PathNode[][] map;
+	private static PriorityQueue<PathNode> openSet;
+	private static ArrayList<PathNode> closedSet;
+	
+	public static void setWorld(World world) {
+		Pathfinding.world = world;
+		dim = world.getLevel().getDimensions();
+	}
+	
+	public static void setPathAsync(Vector start, Vector goal, Pathfinder p) {	
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ArrayDeque<PathNode> stack = findPath(start, goal);
+				if (stack != null) {
+					p.setPath(stack);
+				}
+			}
+		}).start();
+	}
+	
+	//TODO: optimize this using the map double array instead of a random ass set
+	//TODO: can still tweak a bit to introduce more optimal routes, random variation, diagonal movement, etc.
+	public static ArrayDeque<PathNode> findPath(Vector start, Vector goal) {
+				
+		map = new PathNode[dim.getX()][dim.getY()];
+		openSet = new PriorityQueue<PathNode>();
+		closedSet = new ArrayList<PathNode>();
+		
+		map[start.getX()][start.getY()] = new PathNode(start, 0, goal);
+		openSet.offer(map[start.getX()][start.getY()]);
+		
+		PathNode current;
+		while (! openSet.isEmpty()) {
+			
+			current = openSet.poll();
+						
+			if (current.getGridPos().equals(goal)) {
+				return reconstructPath(current);
+			}
+			
+			closedSet.add(current);
+			
+			//getNeighbors has the side effect of creating PathNodes which are null
+			for (PathNode neighbor : current.getNeighbors(map, world.getLevel())) {
+				
+				if (closedSet.contains(neighbor)) {
+					continue;
+				}
+				
+				double tentativeG = current.getG() + current.distance(neighbor);
+				if (! openSet.contains(neighbor)) {
+					openSet.offer(neighbor);
+				}
+				else if (tentativeG >= neighbor.getG()) {
+					continue;
+				}
+				
+				neighbor.setOrigin(current);
+				neighbor.updateHeuristics(tentativeG, goal);
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public static ArrayDeque<PathNode> reconstructPath(PathNode current) {
+		
+		ArrayDeque<PathNode> stack = new ArrayDeque<PathNode>();
+		while (current != null) {
+			stack.add(current);
+			current = current.getOrigin();
+		}
+		return stack;
+		
+	}
+	
+	public PathNode getNode(Vector pos) {
+		return map[pos.getX()][pos.getY()];
+	}
+	
+}
