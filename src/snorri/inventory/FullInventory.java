0cc43bf883e748ba41d5ebcc9b7c671e2cfe860c
Will M <vikingarnir.will@gmail.com>
2016-06-16 03:06:55 -0400

    thought about how to make the FullInventory

---
+++ src/snorri/inventory/FullInventory.java
@@ -0,0 +1,17 @@
+package snorri.inventory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class FullInventory extends ArrayList<Item> implements Serializable {
+
+	/**
+	 * Class that represents a player's full inventory
+	 * Not much to it, since it basically just inherits everything from ArrayList
+	 * Should be filterable (ask Will for enchanting interface design)
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//Toby do stuff here!!
+
+}
