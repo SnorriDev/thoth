cde4eb57f5fcde7c1bf41be012ec9ce48a7b4b31
Toby Mac <tobymac@umich.edu>
2016-11-06 18:48:15 -0500

    starting

--- src/snorri/entities/Crocodile.java
+++ src/snorri/entities/Crocodile.java
@@ -1,10 +1,17 @@
 package snorri.entities;
 
+import java.util.ArrayDeque;
+
 import snorri.animations.Animation;
 import snorri.collisions.RectCollider;
+import snorri.inventory.Carrier;
+import snorri.inventory.Inventory;
+import snorri.pathfinding.PathNode;
+import snorri.pathfinding.Pathfinder;
+import snorri.pathfinding.Targetter;
 import snorri.world.Vector;
 
-public class Crocodile extends Unit {
+public class Crocodile extends Unit implements Pathfinder, Carrier, Targetter {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -14,6 +21,24 @@ public class Crocodile extends Unit {
 	public Crocodile(Vector pos) {
 		super(pos, new RectCollider(143, 57));
 		animation = new Animation(IDLE);
+	}
+
+	@Override
+	public void setTarget(Entity target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Inventory getInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPath(ArrayDeque<PathNode> stack) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
