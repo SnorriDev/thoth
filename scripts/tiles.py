7573c1c55229be0aad29dbcbf5b87ed1ef91222d
William Merrill <vikingarnir.will@gmail.com>
2017-05-13 12:18:13 -0400

    added script to generate all tile possibilities

--- scripts/tiles.py
+++ scripts/tiles.py
@@ -1,18 +1,22 @@
 import Image
 
-PATH = "../textures/tiles/background/"
+IN_PATH = "baseTiles/"
+OUT_PATH = "mixedTiles/"
 NEW_SIZE = 32
+NUM_TILES = 13
 
-tile1 = raw_input("tile1: ")
-tile2 = raw_input("tile2: ")
-t1 = Image.open(PATH + tile1 + ".png")
-t2 = Image.open(PATH + tile2 + ".png")
-t1.thumbnail((NEW_SIZE, NEW_SIZE), Image.ANTIALIAS)
-t2.thumbnail((NEW_SIZE, NEW_SIZE), Image.ANTIALIAS)
+int2texture = lambda z: "floor%02d" % (z,)
 
-new = Image.new("RGBA", (64, 64), (255, 255, 255, 255))
-for x in range(0, 64, NEW_SIZE):
-	for y in range(0, 64, NEW_SIZE):
-		new.paste(t1 if (x + y) / NEW_SIZE % 2 == 0 else t2, (x, y))
+tiles = [Image.open(IN_PATH + int2texture(i) + ".png") for i in range(NUM_TILES)]
+for tile in tiles:
+	tile.thumbnail((NEW_SIZE, NEW_SIZE), Image.ANTIALIAS)
 
-new.save(tile1 + "-" + tile2 + ".png")
+for i in range(NUM_TILES):
+	for j in range(NUM_TILES):
+
+		new = Image.new("RGBA", (64, 64), (255, 255, 255, 255))
+		for x in range(0, 64, NEW_SIZE):
+			for y in range(0, 64, NEW_SIZE):
+				new.paste(tiles[i if (x + y) / NEW_SIZE % 2 == 0 else j], (x, y))
+
+		new.save(OUT_PATH + int2texture(i) + "-" + int2texture(j) + ".png")
