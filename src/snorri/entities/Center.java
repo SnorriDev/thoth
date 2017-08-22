f66ccdb830e22893e4a09c42e70047f05655d44f
Toby Mac <tobymac@umich.edu>
2017-08-22 15:48:39 -0400

    also added a smiley face temporary textur and made tree into "black"

---
+++ src/snorri/entities/Center.java
@@ -0,0 +1,22 @@
+package snorri.entities;
+
+import snorri.animations.Animation;
+import snorri.collisions.CircleCollider;
+import snorri.collisions.Collider;
+import snorri.world.Vector;
+
+public class Center extends Entity {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Animation ANIMATION = new Animation("/textures/objects/center00.png");
+
+	
+	public Center(Vector pos) {
+		super(pos, new CircleCollider(16));
+		ignoreCollisions = true;
+		staticObject = true;
+		animation = new Animation(ANIMATION);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
