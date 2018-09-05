8ce6f1fb41d9b77cb2c47000c98067563b0a8500
Will M <vikingarnir.will@gmail.com>
2018-09-05 00:12:20 -0400

    First attempts at ai.

---
+++ src/snorri/ai/AIAgent.java
@@ -0,0 +1,29 @@
+package snorri.ai;
+
+import snorri.entities.Entity;
+import snorri.world.Vector;
+import snorri.world.World;
+
+/** Interface for things that can have AI added to them.
+ * @author lambdaviking
+ *
+ * Generally, this will apply to Units.
+ */
+public interface AIAgent {
+	
+	/** Get the position of the agent in global coordinates. */
+	Vector getPos();
+	
+	/** Set the target of this agent. */
+	void setTarget(Entity target);
+	
+	/** Get the target of this agent. */
+	Entity getTarget();
+	
+	/** Return true if the agent can currently attack target and false otherwise. */
+	boolean canAttack(Entity target, World world);
+	
+	/** Execute an attack target. */
+	void attack(Entity target, World world);
+
+}
